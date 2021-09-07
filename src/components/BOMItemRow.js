16d14b4d98fab85f84afbd04f63a4b3fa9cab84d
unknown <laz51@pitt.edu>
2021-09-07 02:05:08 -0400

    Fixed delete not workign instantly

--- src/components/BOMItemRow.js
+++ src/components/BOMItemRow.js
@@ -9,39 +9,51 @@ class BomItemRow extends React.Component {
             showItem: true,
             bomItem: this.props.item,
         };
+        this.toggleItem = this.toggleItem.bind(this);
+    }
+
+    toggleItem() {
+        this.setState({
+            showItem: false,
+        });
     }
 
     render() {
         /*Renders out buttons and contents of bom item passed in */
         return (
             <>
-                <td>
-                    <DeleteBOM
-                        id={this.state.bomItem.id}
-                        bomId={this.state.bomItem.bomId}
-                        url={this.props.url}
-                        url_template={this.props.url_template}
-                    />
-                </td>
-                <td>{this.state.bomItem.bomId}</td>
-                <td>{this.state.bomItem.id}</td>
-                <td>{this.state.bomItem.item_unit_cost}</td>
-                <td>{this.state.bomItem.quantity}</td>
-                <td>
-                    {this.state.bomItem.item_unit_cost *
-                        this.state.bomItem.quantity}
-                </td>
-                <td>{this.state.bomItem.specific_part}</td>
-                <td>
-                    {new Date(
-                        this.state.bomItem.created_at * 1000
-                    ).toLocaleString()}
-                </td>
-                <td>
-                    {new Date(
-                        this.state.bomItem.updated_at * 1000
-                    ).toLocaleString()}
-                </td>
+                {this.state.showItem ? (
+                    <>
+                        <td>
+                            <DeleteBOM
+                                id={this.state.bomItem.id}
+                                bomId={this.state.bomItem.bomId}
+                                url={this.props.url}
+                                url_template={this.props.url_template}
+                                parentMethod={this.toggleItem}
+                            />
+                        </td>
+                        <td>{this.state.bomItem.bomId}</td>
+                        <td>{this.state.bomItem.id}</td>
+                        <td>{this.state.bomItem.item_unit_cost}</td>
+                        <td>{this.state.bomItem.quantity}</td>
+                        <td>
+                            {this.state.bomItem.item_unit_cost *
+                                this.state.bomItem.quantity}
+                        </td>
+                        <td>{this.state.bomItem.specific_part}</td>
+                        <td>
+                            {new Date(
+                                this.state.bomItem.created_at * 1000
+                            ).toLocaleString()}
+                        </td>
+                        <td>
+                            {new Date(
+                                this.state.bomItem.updated_at * 1000
+                            ).toLocaleString()}
+                        </td>
+                    </>
+                ) : null}
             </>
         );
     }
